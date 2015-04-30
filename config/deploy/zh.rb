ac6e28fa9f8dbcbb4313087f69931bed84d89603
MingYong Yu <IdeaMan.Yu@gmail.com>
2015-04-30 23:32:45 +0800

    Update zh.rb

--- config/deploy/zh.rb
+++ config/deploy/zh.rb
@@ -1,6 +1,6 @@
-set :domain, 'yafeilee.me'
-set :deploy_to, '/home/ruby/wblog'
-set :repository, 'git@github.com:windy/wblog.git'
+set :domain, '106.185.53.80'
+set :deploy_to, '/home/ideaman/wblog'
+set :repository, 'git@github.com:IdeaManYu/wblog.git'
 set :branch, 'master'
-set :user, 'ruby'
+set :user, 'ideaman'
 set :unicorn_config, -> { "#{deploy_to}/#{current_path}/config/unicorn/zh.rb" }
