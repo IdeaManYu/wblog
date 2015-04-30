909f1cab26c82753a83055db331b09cc6845cd0c
MingYong Yu <IdeaMan.Yu@gmail.com>
2015-04-30 23:32:02 +0800

    Update en.rb

--- config/deploy/en.rb
+++ config/deploy/en.rb
@@ -1,6 +1,6 @@
-set :domain, 'yafeilee.me'
-set :deploy_to, '/home/ruby/wblog_en'
-set :repository, 'git@github.com:windy/wblog.git'
+set :domain, '106.185.53.80'
+set :deploy_to, '/home/ideaman/wblog_en'
+set :repository, 'git@github.com:ideaman/wblog.git'
 set :branch, 'master'
-set :user, 'ruby'
+set :user, 'ideaman'
 set :unicorn_config, -> { "#{deploy_to}/#{current_path}/config/unicorn/en.rb" }
