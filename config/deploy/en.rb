a451644e36f35a9509c8f98ad8fa266d50a58b03
MingYong Yu <IdeaMan.Yu@gmail.com>
2015-04-30 23:33:06 +0800

    Update en.rb

--- config/deploy/en.rb
+++ config/deploy/en.rb
@@ -1,6 +1,6 @@
 set :domain, '106.185.53.80'
 set :deploy_to, '/home/ideaman/wblog_en'
-set :repository, 'git@github.com:ideaman/wblog.git'
+set :repository, 'git@github.com:IdeaManYu/wblog.git'
 set :branch, 'master'
 set :user, 'ideaman'
 set :unicorn_config, -> { "#{deploy_to}/#{current_path}/config/unicorn/en.rb" }
